73ddd1879ed345f147ae43d6fa213d73e2015fa2
Jason <11249195+jcapili@users.noreply.github.com>
2020-06-06 16:16:11 -0700

    First commit

---
+++ driver.py
@@ -0,0 +1,8 @@
+from calculator import CrowPy
+
+cp = CrowPy()
+cp.calculateCSVMiles('~/Downloads/aprilOrderData.csv', '~/Downloads/april.csv', False, False)
+
+# print calculator.calculateMiles('9405509206119500349046', True)
+
+# Errors: [9405509206119500349046]
